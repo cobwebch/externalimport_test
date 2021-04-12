c21e47ab33ecd93509180b412f53080ddf02085a
Francois Suter <sutfra@gmail.com>
2021-04-12 11:10:19 +0200

    Replace usage of MM property for bundles

--- Configuration/TCA/tx_externalimporttest_bundle.php
+++ Configuration/TCA/tx_externalimporttest_bundle.php
@@ -117,12 +117,15 @@ return [
                         'external' => [
                                 0 => [
                                         'field' => 'product',
-                                        'MM' => [
-                                                'mapping' => [
-                                                        'table' => 'tx_externalimporttest_product',
-                                                        'referenceField' => 'sku'
-                                                ],
-                                                'sorting' => 'position'
+                                        'multipleRows' => true,
+                                        'multipleSorting' => 'position',
+                                        'transformations' => [
+                                                10 => [
+                                                        'mapping' => [
+                                                                'table' => 'tx_externalimporttest_product',
+                                                                'referenceField' => 'sku'
+                                                        ]
+                                                ]
                                         ]
                                 ]
                         ]
