928a1db27614cb7f56c47bccc496d5b19eac1d56
Francois Suter <>
2021-10-05 23:13:01 +0200

    Raise version to 0.10.0 and update requirements

--- ext_tables.php
+++ ext_tables.php
@@ -1,7 +1,4 @@
 <?php
-if (!defined ('TYPO3_MODE')) {
-    die ('Access denied.');
-}
 
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_externalimporttest_product');
 \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::allowTableOnStandardPages('tx_externalimporttest_store_product');
