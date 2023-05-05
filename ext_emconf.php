d8f83fb6e09012387b05c4dc6470200dbaeff96d
Francois Suter <sutfra@gmail.com>
2023-05-05 17:44:49 +0200

    Require external_import 7 or develop branch, raise version to 0.12.0

--- ext_emconf.php
+++ ext_emconf.php
@@ -5,7 +5,7 @@ $EM_CONF[$_EXTKEY] = [
     'description' => 'Test data and scenarios for the External Import extension.',
     'category' => 'example',
     'author' => 'Francois Suter (Idéative)',
-    'author_email' => 'typo3@cobweb.ch',
+    'author_email' => 'typo3@ideative.ch',
     'shy' => '',
     'priority' => '',
     'module' => '',
@@ -17,16 +17,16 @@ $EM_CONF[$_EXTKEY] = [
     'clearCacheOnLoad' => 1,
     'lockType' => '',
     'author_company' => '',
-    'version' => '0.11.0',
+    'version' => '0.12.0',
     'constraints' =>
         [
             'depends' =>
                 [
-                    'external_import' => '6.3.0-0.0.0',
+                    'external_import' => '7.0.0-0.0.0',
                     'svconnector_feed' => '4.0.0-0.0.0',
                     'svconnector_csv' => '4.0.0-0.0.0',
                     'svconnector_json' => '4.0.0-0.0.0',
-                    'typo3' => '11.5.0-12.5.99',
+                    'typo3' => '12.4.0-12.4.99',
                 ],
             'conflicts' =>
                 [
