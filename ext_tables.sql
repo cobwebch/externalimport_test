ae0ae0db7fcd8320cdb94fc87c6c6cde8f819928
Francois Suter <sutfra@gmail.com>
2025-02-24 18:15:55 +0100

    Clean up SQL definitions

--- ext_tables.sql
+++ ext_tables.sql
@@ -2,61 +2,61 @@
 # Table structure for products
 #
 CREATE TABLE tx_externalimporttest_product (
-	sku varchar(255) DEFAULT '' NOT NULL,
-	name varchar(255) DEFAULT '' NOT NULL,
-	path_segment varchar(2048) DEFAULT '' NOT NULL,
-	tags varchar(255) DEFAULT '' NOT NULL,
+	sku varchar(255) default '' not null,
+	name varchar(255) default '' not null,
+	path_segment varchar(2048) default '' not null,
+	tags varchar(255) default '' not null,
 	attributes text,
-	stores int(11) DEFAULT '0' NOT NULL,
-	categories int(11) DEFAULT '0' NOT NULL,
-	pictures int(11) DEFAULT '0' NOT NULL,
-	designers int(11) DEFAULT '0' NOT NULL,
-	created int(11) DEFAULT '0' NOT NULL
+	stores int(11) default 0 not null,
+	categories int(11) default 0 not null,
+	pictures int(11) default 0 not null,
+	designers int(11) default 0 not null,
+	created int(11) default 0 not null
 );
 
 #
 # Table structure for tags
 #
 CREATE TABLE tx_externalimporttest_tag (
-	code varchar(20) DEFAULT '' NOT NULL,
-	name varchar(255) DEFAULT '' NOT NULL
+	code varchar(20) default '' not null,
+	name varchar(255) default '' not null
 );
 
 #
 # Table structure for designers
 #
 CREATE TABLE tx_externalimporttest_designer (
-	name varchar(255) DEFAULT '' NOT NULL,
-	code varchar(20) DEFAULT '' NOT NULL,
-	products int(11) DEFAULT '0' NOT NULL,
-	picture int(11) DEFAULT '0' NOT NULL
+	name varchar(255) default '' not null,
+	code varchar(20) default '' not null,
+	products int(11) default 0 not null,
+	picture int(11) default 0 not null
 );
 
 CREATE TABLE tx_externalimporttest_product_designer_mm (
-	uid_local int(11) DEFAULT '0' NOT NULL,
-	uid_foreign int(11) DEFAULT '0' NOT NULL,
-	sorting int(11) DEFAULT '0' NOT NULL,
-	sorting_foreign int(11) DEFAULT '0' NOT NULL
+	uid_local int(11) default 0 not null,
+	uid_foreign int(11) default 0 not null,
+	sorting int(11) default 0 not null,
+	sorting_foreign int(11) default 0 not null
 );
 
 #
 # Table structure for orders
 #
 CREATE TABLE tx_externalimporttest_order (
-	order_date int(11) DEFAULT '0' NOT NULL,
-	products int(11) DEFAULT '0' NOT NULL,
-	client_id varchar(255) DEFAULT '' NOT NULL,
-	order_id varchar(255) DEFAULT '' NOT NULL
+	order_date int(11) default 0 not null,
+	products int(11) default 0 not null,
+	client_id varchar(255) default '' not null,
+	order_id varchar(255) default '' not null
 );
 
 #
 # Table structure for order details
 #
 CREATE TABLE tx_externalimporttest_order_items (
-	uid_local int(11) DEFAULT '0' NOT NULL,
-	uid_foreign int(11) DEFAULT '0' NOT NULL,
-	sorting_foreign int(11) DEFAULT '0' NOT NULL,
-	quantity int(11) DEFAULT '0' NOT NULL,
+	uid_local int(11) default 0 not null,
+	uid_foreign int(11) default 0 not null,
+	sorting_foreign int(11) default 0 not null,
+	quantity int(11) default 0 not null,
 
 	KEY uid_local (uid_local),
 	KEY uid_foreign (uid_foreign)
@@ -66,10 +66,10 @@ CREATE TABLE tx_externalimporttest_order_items (
 # Table structure for bundles
 #
 CREATE TABLE tx_externalimporttest_bundle (
-	name varchar(255) DEFAULT '' NOT NULL,
-	products int(11) DEFAULT '0' NOT NULL,
-	bundle_code char(10) DEFAULT '' NOT NULL,
-	maker varchar(255) DEFAULT '' NOT NULL,
+	name varchar(255) default '' not null,
+	products int(11) default 0 not null,
+	bundle_code char(10) default '' not null,
+	maker varchar(255) default '' not null,
 	notes varchar(255) DEFAULT NULL
 );
 
@@ -77,9 +77,9 @@ CREATE TABLE tx_externalimporttest_bundle (
 # Table structure for order bundle items
 #
 CREATE TABLE tx_externalimporttest_bundle_product_mm (
-	uid_local int(11) DEFAULT '0' NOT NULL,
-	uid_foreign int(11) DEFAULT '0' NOT NULL,
-	sorting int(11) DEFAULT '0' NOT NULL,
+	uid_local int(11) default 0 not null,
+	uid_foreign int(11) default 0 not null,
+	sorting int(11) default 0 not null,
 
 	KEY uid_local (uid_local),
 	KEY uid_foreign (uid_foreign)
@@ -89,40 +89,40 @@ CREATE TABLE tx_externalimporttest_bundle_product_mm (
 # Table structure for stores
 #
 CREATE TABLE tx_externalimporttest_store (
-	name varchar(255) DEFAULT '' NOT NULL,
-	store_code char(10) DEFAULT '' NOT NULL,
-	products int(11) DEFAULT '0' NOT NULL
+	name varchar(255) default '' not null,
+	store_code char(10) default '' not null,
+	products int(11) default 0 not null
 );
 
 #
 # Table structure for store stocks
 #
 CREATE TABLE tx_externalimporttest_store_product (
-	store int(11) DEFAULT '0' NOT NULL,
-	product int(11) DEFAULT '0' NOT NULL,
-	stock int(11) DEFAULT '0' NOT NULL
+	store int(11) default 0 not null,
+	product int(11) default 0 not null,
+	stock int(11) default 0 not null
 );
 
 #
 # Table structure for invoices
 #
 CREATE TABLE tx_externalimporttest_invoice (
-	invoice_id varchar(255) DEFAULT '' NOT NULL,
-	order_id int(11) DEFAULT '0' NOT NULL,
-	amount double(11,2) DEFAULT '0.0' NOT NULL,
-	currency char(3) DEFAULT 'USD' NOT NULL
+	invoice_id varchar(255) default '' not null,
+	order_id int(11) default 0 not null,
+	amount double(11,2) default 0.0 not null,
+	currency char(3) default 'USD' not null
 );
 
 #
 # Extend pages table for product-to-pages import
 #
 CREATE TABLE pages (
-	product_sku varchar(255) DEFAULT '' NOT NULL
+	product_sku varchar(255) default '' not null
 );
 
 #
 # Extend sys_category table for product categories import
 #
 CREATE TABLE sys_category (
-	external_key varchar(255) DEFAULT '' NOT NULL
+	external_key varchar(255) default '' not null
 );
