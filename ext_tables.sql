57431c50236d503534f0627d5f81b79fb010941f
Francois Suter <sutfra@gmail.com>
2026-08-09 10:37:04 +0200

    Fix column creation for testing

--- ext_tables.sql
+++ ext_tables.sql
@@ -1,3 +1,4 @@
+# Weirdly this column is not created when running automated tests, so we create it manually
 CREATE TABLE tx_externalimporttest_order_items (
     sorting_foreign int(11) default 0 not null
 );
